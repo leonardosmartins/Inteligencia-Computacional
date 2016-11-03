c6a7859312648dfd2e88027d4f4f9f1da4942083
Leonardo Martins <leonardosilva_6@hotmail.com>
2016-11-02 23:10:26 -0200

    Day3

--- Trabalho2/Code/RedesNeurais.py
+++ Trabalho2/Code/RedesNeurais.py
@@ -1,17 +1,18 @@
 import time
+import sys
 
-def readFile(fileName):
-	file = open('../Input/'+fileName, 'r')
+def readFile(patchFile, fileName):
+	file = open('../'+patchFile+'/'+fileName, 'r')
 	body = file.read()
 	file.close()
-	inputZero = range(31) 
-	inputZero[30] = 0
+	inputFile = range(31) 
+	inputFile[30] = 0
 
 	for i in range(30):
-		inputZero[i] = int (body[i*2])
+		inputFile[i] = int (body[i*2])
 
-	return inputZero
-
+	return inputFile
+		
 def createMatrix(lin, col):
 	matrix = []
 	for i in range(lin):
@@ -19,23 +20,23 @@ def createMatrix(lin, col):
 
 	return matrix
 
-def fillMatrix():
-	for i in range(6):
-		matrix[i] = readFile(str(i)+'.txt')
+def fillMatrix(size):
+	for i in range(size):
+		trainningMatrix[i] = readFile('Trainning-Input', str(i)+'.txt')
 
-	return matrix	
+	return trainningMatrix	
 
-def train(neuronio,weightMatrix, matrix):
-	verifyInput = range(6)
+def train(neuronio,weightMatrix, trainningMatrix):
+	verifyInput = range(len(trainningMatrix))
 	verifyEnd = 1
 	epoch = 0
 
 	while(verifyEnd != 0):
 		epoch += 1
-		for j in range(6):
+		for j in range(len(trainningMatrix)):
 			result = 0
 			for i in range(len(weightMatrix[0])):
-				result = result + weightMatrix[neuronio][i] * matrix[j][i]
+				result = result + weightMatrix[neuronio][i] * trainningMatrix[j][i]
 
 			if result > 0:
 				result = 1
@@ -44,13 +45,13 @@ def train(neuronio,weightMatrix, matrix):
 	
 			if result == 0 and j == neuronio:
 				for i in range(len(weightMatrix[0])):
-					weightMatrix[neuronio][i] = weightMatrix[neuronio][i] + matrix[j][i]
+					weightMatrix[neuronio][i] = weightMatrix[neuronio][i] + trainningMatrix[j][i]
 
 				verifyInput[j] = 1			
 
 			elif result == 1 and j != neuronio:		
 				for i in range(len(weightMatrix[0])):
-					weightMatrix[neuronio][i] = weightMatrix[neuronio][i] - matrix[j][i]
+					weightMatrix[neuronio][i] = weightMatrix[neuronio][i] - trainningMatrix[j][i]
 				
 				verifyInput[j] = 1			
 
@@ -61,40 +62,108 @@ def train(neuronio,weightMatrix, matrix):
 		for i in range(len(verifyInput)):
 			verifyEnd = verifyEnd + verifyInput[i] 					
 	
-	print "Neuronio",neuronio,"Epocas necessarias para treinar:",epoch
+	print "Neuronio",neuronio,"\nEpocas necessarias para treinar:",epoch, "\nVetor de pesos:",weightMatrix[neuronio],'\n\n'
 	return weightMatrix			
 
-def verifyNeuronio(weightMatrix, matrix, neuronio):
-	for j in range(6):
+def verifyNeuronio(weightMatrix, matrixInput, neuronio, inputName1, inputName2):
+	result = 0
+	for i in range(len(weightMatrix[0])):
+		result = result + weightMatrix[neuronio][i] * matrixInput[i]
+
+	if result > 0:
+		result = 1
+	else:
 		result = 0
-		for i in range(len(weightMatrix[0])):
-			result = result + weightMatrix[neuronio][i] * matrix[j][i]
 
-		if result > 0:
-			result = 1
-		else:
-			result = 0
+	print "Neuronio:",neuronio, "<==>", "Entrada:", inputName1, "-", inputName2, "<==>", "Resultado",result			
 
-		print "Neuronio:",neuronio,"Entrada:",j,"Resultado",result			
+if int(sys.argv[1]) == 1:
+	#EXERCISE 1
+	weightMatrix = createMatrix(2,31)
+	trainningMatrix = createMatrix(2,31)
+	trainningMatrix = fillMatrix(2)
+	matrixInput = range(31)
 
-matrix = createMatrix(6,31)
-matrix = fillMatrix()
+	for i in range(len(weightMatrix)):
+		weightMatrix[i][30] = 1
 
-weightMatrix = createMatrix(6,31)
+	weightMatrix = train(1,weightMatrix,trainningMatrix)
 
-weightMatrix[0][30] = 1
-weightMatrix[1][30] = 1
-weightMatrix[2][30] = 1
-weightMatrix[3][30] = 1
-weightMatrix[4][30] = 1
-weightMatrix[5][30] = 1
+	for i in range(6):
+		print '\n'
+		for j in range(10):
+			matrixInput = readFile('Input', str(i)+'-'+str(j)+'.txt')
+			verifyNeuronio(weightMatrix,matrixInput,1,i,j)
 
-for i in range(len(weightMatrix)):
-	weightMatrix = train(i,weightMatrix,matrix)
+elif int(sys.argv[1]) == 2:
+	#EXERCISE 2
+	weightMatrix = createMatrix(2,31)
+	trainningMatrix = createMatrix(2,31)
+	trainningMatrix = fillMatrix(2)
+	matrixInput = range(31)
 
-for i in range(len(weightMatrix)):
-	verifyNeuronio(weightMatrix,matrix,i)
-#print(weightMatrix)
+	for i in range(len(weightMatrix)):
+		weightMatrix[i][30] = 1
 
-# print(matrix)	
+	weightMatrix = train(0,weightMatrix,trainningMatrix)
+	weightMatrix = train(1,weightMatrix,trainningMatrix)
 
+	for i in range(6):
+		print '\n'
+		for j in range(10):
+			matrixInput = readFile('Input', str(i)+'-'+str(j)+'.txt')
+			verifyNeuronio(weightMatrix,matrixInput,0,i,j)		
+			verifyNeuronio(weightMatrix,matrixInput,1,i,j)
+
+elif int(sys.argv[1]) == 3:
+	#EXERCISE 3
+	weightMatrix = createMatrix(6,31)
+	trainningMatrix = createMatrix(6,31)
+	trainningMatrix = fillMatrix(6)
+	matrixInput = range(31)
+
+	for i in range(len(weightMatrix)):
+		weightMatrix[i][30] = 1
+
+	for i in range(len(weightMatrix)):
+		weightMatrix = train(i,weightMatrix,trainningMatrix)
+
+	for i in range(len(weightMatrix)):
+		for j in range(10):
+			matrixInput = readFile('Input', str(i)+'-'+str(j)+'.txt')
+			print '\n'
+			for k in range(len(weightMatrix)):
+				verifyNeuronio(weightMatrix,matrixInput,k,i,j)
+
+	matrixInput = readFile('Input', 'A.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'A','A')
+
+	matrixInput = readFile('Input', 'E.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'E','E')
+
+	matrixInput = readFile('Input', 'T.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'T','T')		
+
+	matrixInput = readFile('Input', 'H.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'H','H')
+
+	matrixInput = readFile('Input', 'C.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'C','C')
+
+	matrixInput = readFile('Input', 'N.txt')
+	print '\n'
+	for i in range(len(weightMatrix)):
+		verifyNeuronio(weightMatrix,matrixInput,i,'N','N')
+
+else: 
+	print "This exercise doesn't exist"				
